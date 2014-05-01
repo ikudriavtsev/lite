a810def0e071c79553360eea6d37a8a81596128e
Iurii Kudriavtsev <iurii.kudriavtsev@gmail.com>
2014-05-01 17:21:11 +0200

    Adding time to empty/full for battery.

--- app.py
+++ app.py
@@ -17,14 +17,15 @@ app.config.update(dict(
 def index():
     present = is_batery_present()
     if not present:
-        capacity, power_cord = None, None
+        capacity, power_cord, battery_time = None, None, None
     else:
-        capacity, power_cord = get_battery_status()
+        capacity, power_cord, battery_time = get_battery_status()
     return render_template(
         'index.html',
         capacity=capacity,
         power_cord=power_cord,
-        present=present
+        present=present,
+        battery_time=battery_time
     )
 
 
@@ -33,14 +34,15 @@ def battery():
     if request.is_xhr:
         present = is_batery_present()
         if not present:
-            capacity, power_cord = None, None
+            capacity, power_cord, battery_time = None, None, None
         else:
-            capacity, power_cord = get_battery_status()
+            capacity, power_cord, battery_time = get_battery_status()
         return render_template(
             'battery.html',
             capacity=capacity,
             power_cord=power_cord,
-            present=present
+            present=present,
+            battery_time=battery_time
         )
     return abort(404)
 
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 import platform
+from subprocess import Popen, PIPE
+import re
 
 
 # the directory that contains the info about the battery
@@ -9,10 +11,10 @@ battery_info_dir = '/sys/class/power_supply/BAT0'
 
 def get_battery_status():
     """Reads the current battery status.
-    Returns the capacity and the status of power cord.
+    Returns the capacity, the status of power cord and the time.
     Returns tuple of None if the info is not available.
 
-    :return: tuple of capacity and power cord or tuple of None
+    :return: tuple of capacity, power cord and time or tuple of None
     :rtype: tuple
     """
     capacity_file_path = battery_info_dir + '/capacity'
@@ -21,13 +23,48 @@ def get_battery_status():
        not os.path.exists(capacity_file_path) or\
        not os.path.exists(status_file_path):
         return (None, None)
+    battery_time = None
     with open(capacity_file_path, 'r') as f:
         capacity = f.read().strip()
     with open(status_file_path, 'r') as f:
         power_cord = f.read().strip().lower()
         if power_cord not in ('charging', 'discharging'):
             power_cord = 'using a power cord'
-    return capacity, power_cord
+        else:
+            t = get_battery_time()
+            total_mins = t*60
+            hours = int(total_mins)/60
+            mins = int(total_mins) - hours*60
+            hours_label = 'hour' if hours == 1 else 'hours'
+            mins_label = 'minute' if mins == 1 else 'minutes'
+            battery_time = '%s %s %s %s' % (
+                hours,
+                hours_label,
+                mins,
+                mins_label
+            )
+    return capacity, power_cord, battery_time
+
+
+def get_battery_time():
+    """Returns battery remaining/charging time.
+    Uses `upower` shell command.
+
+    :return: battery time to full or time to empty
+    :rtype: float or NoneType
+    """
+    upower = Popen(
+        ['upower', '-i', '/org/freedesktop/UPower/devices/battery_BAT0'],
+        stdout=PIPE
+    )
+    grep = Popen(['grep', '-E', 'time\ to'], stdin=upower.stdout, stdout=PIPE)
+    std, err = grep.communicate()
+    if not err:
+        regexp = '^time to \w+:\s+?(\d+,\d+) hours$'
+        match = re.match(regexp, std.strip())
+        groups = match.groups()
+        if groups:
+            return float(groups[0].replace(',', '.'))
 
 
 def is_batery_present():
